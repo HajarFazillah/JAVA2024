9a68d4a7135cff2c47db1b4e6ff018c30cd51693
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-10-01 23:51:45 +0900

    Var.java

---
+++ 예제0202/src/예제0202/Var.java
@@ -0,0 +1,16 @@
+// 예제02-02 : var 키워드를 사용하여 변수 선언
+
+package 예제0202;
+
+public class Var {
+	
+	public static void main(String[] args) {
+		var price = 200;
+		var name = "yoomin";
+		var pi = 3.14;
+		
+		System.out.println("price = "+(price+1000));
+		System.out.println("name = "+name);
+		System.out.println("pi = "+pi*10);
+	}
+}
