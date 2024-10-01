672c3ddb3d3b326c5095001874cf2826d755f5ed
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-10-02 01:47:17 +0900

    TernaryOperator.java

---
+++ 예제0209/src/예제0209/TernaryOperator.java
@@ -0,0 +1,15 @@
+// 예제 02-09 : 조건 연산자 사용하기
+
+package 예제0209;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 5;
+		System.out.println("드 수의 차는 "+((a>b)?(a-b):(b-a)));
+
+	}
+
+}
