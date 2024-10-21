27e50ec4e1adc44060e90e051c090584e1f55649
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-10-21 15:09:55 +0900

    Code comments updated.

--- 예제0403/src/예제0403/Circle.java
+++ 예제0403/src/예제0403/Circle.java
@@ -5,31 +5,42 @@ package 예제0403;
 
 public class Circle {
 	
+	// fields to store the radius and name of the circle
+	// that will be used later
 	int radius;
 	String name;
 
-	public Circle() { // 매개 변수 없는 생성자
-		radius = 1; name = ""; // radius의 초기값은 1
+	// constructor with no parameters
+	public Circle() {	// 매개 변수 없는 생성자
+		radius = 1;		// default radius의 초기값은 1
+		name = "";		// default name to an empty string
 	}
+
+	// constructor with parameters
 	public Circle(int r, String n) { // 매개 변수를 가진 생성자
-		radius = r; name = n;
+		radius = r;		// set radius to the value of r passed in
+		name = n;		// set name to the value of n passed in
 	}
+
+	// method to be used later for calculationg the area of circle
 	public double getArea() {
-		return 3.14*radius*radius;
+		return 3.14*radius*radius;	// area formula written here
+		// Area = π * radius * radius
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		// create a Circle object named "pizza" with a radius of 10 and its name 자바피자
 		Circle pizza = new Circle(10, "자바피자"); // Circle 객체 생성, 반지름 10
-		double area = pizza.getArea();
-		System.out.println(pizza.name + "의 면적은 " + area);
+		double area = pizza.getArea(); // to calculate the pizza 자바피자 area
+		System.out.println(pizza.name + "의 면적은 " + area); // show output
 
-		Circle donut = new Circle(); // Circle 객체 생성, 반지름 1
-		donut.name = "도넛피자";
-		area = donut.getArea();
+		// create a Circle object named 'donut'
+		Circle donut = new Circle(); 	// Circle 객체 생성, 반지름 1
+		donut.name = "도넛피자";		 // only name specifically given, default radius being 1
+		area = donut.getArea();			// calculate area of donut
 		
-		System.out.println(donut.name + "의 면적은 " + area);
+		System.out.println(donut.name + "의 면적은 " + area); // show output
 		
 	}
 }
