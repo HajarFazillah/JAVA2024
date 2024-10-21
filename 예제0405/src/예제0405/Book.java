063936b6f22aa5d66ea78d548a62a89c9d739504
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-10-21 15:32:21 +0900

    Updated Book.java

--- 예제0405/src/예제0405/Book.java
+++ 예제0405/src/예제0405/Book.java
@@ -2,25 +2,35 @@ package 예제0405;
 
 public class Book {
 	
+	// field to store the title and author of the book
 	String title;
 	String author;
-	void show() { System.out.println(title + " " + author); }
 
-	public Book() {
-		this("", "");
-		System.out.println("생성자 호출됨");
+	// constructor with no parameters
+	public Book() {	
+		this("", ""); // cal the 2 constructor with two parameters (setting default values)
+		System.out.println("생성자 호출됨"); // print message indicating constructor has been called
 	}
 
+	// constructor with one parameter for title
+	// allow program to handle cases where no author's name given
 	public Book(String title) {
 		this(title, "작자미상");
 	}
 
+	// constructor with 2 parameters for title and author
 	public Book(String title, String author) {
-		this.title = title; this.author = author;
+		this.title = title;
+		this.author = author;
+	}
+
+	// display the title and author
+	void show() { 
+		System.out.println(title + " " + author);
 	}
 	
 	public static void main(String [] args) {
-		// TODO Auto-generated method stub
+		
 		Book littlePrince = new Book("어린왕자", "생텍쥐페리");
 		Book loveStory = new Book("춘향전");
 		Book emptyBook = new Book();		
